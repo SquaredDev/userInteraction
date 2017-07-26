64c90d6446d4f65cfd0ee49f6d03180ae92c3ab4
James <boydston.j@gmail.com>
2017-07-26 10:06:59 -0700

    stuff

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+
 
 app.engine('mustache', mustacheExpress());
 app.set('views', './views')
@@ -10,7 +16,22 @@ app.set('view engine', 'mustache')
 app.use(express.static(path.join(__dirname, 'static')))
 
 app.get("/", function(req, res, next){
-  res.render("index", {appType:"Express"})
+  res.render("index", {thankyou:req.query.thankyou})
+})
+
+var users = []
+
+app.post("/user/add", function (req, res, next) {
+  users.push({
+    fname: req.body.fname,
+    lname: req.body.lname
+  })
+  console.log(users);
+  res.redirect("/thankyou")
+})
+
+app.get("/thankyou", function (req, res, next) {
+  res.send("<h1>Thank You</h1>")
 })
 
 app.listen(3000, function(){
